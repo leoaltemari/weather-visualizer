05153f9899f163df44c378c6a9fd50e498166a04
leoaltemari <leo.altemari@gmail.com>
2025-10-13 10:00:31 -0300

    feat: initial project config

---
+++ src/app/app.ts
@@ -0,0 +1,9 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+})
+export class App {
+  protected readonly title = signal('weather-visualizer');
+}
