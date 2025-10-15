481474eac41310e183eee87abc40521a05feb7f9
leoaltemari <leo.altemari@gmail.com>
2025-10-15 15:23:13 -0300

    feat: creates loading spinner component

---
+++ src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -0,0 +1,19 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-loading-spinner',
+  imports: [],
+  template: `
+    <div class="relative h-[{{ size() }}}px] w-[{{ size() }}px]">
+      <div class="spinner-ring"></div>
+      <div class="spinner-ring"></div>
+      <div class="spinner-ring"></div>
+      <div class="loading-icon">{{ icon() }}</div>
+    </div>
+  `,
+  styleUrl: './loading-spinner.component.scss',
+})
+export class LoadingSpinnerComponent {
+  readonly size = input<number>(120);
+  readonly icon = input<string>('🌤️');
+}
