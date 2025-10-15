3a9c4a38f85d1f26b4923d74b076c35cef12b76c
leoaltemari <leo.altemari@gmail.com>
2025-10-15 18:13:10 -0300

    fix: fix class binding at loadin-spinner component

--- src/app/shared/components/loading-spinner/loading-spinner.component.ts
+++ src/app/shared/components/loading-spinner/loading-spinner.component.ts
@@ -1,18 +1,22 @@
-import { Component, input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-loading-spinner',
   template: `
-    <div class="relative h-[{{ size() }}}px] w-[{{ size() }}px]">
+    <div class="relative" [style]="sizeClass()">
       <div class="spinner-ring"></div>
       <div class="spinner-ring"></div>
       <div class="spinner-ring"></div>
       <div class="loading-icon">{{ icon() }}</div>
     </div>
   `,
+  imports: [CommonModule],
   styleUrl: './loading-spinner.component.scss',
 })
 export class LoadingSpinnerComponent {
   readonly size = input<number>(120);
   readonly icon = input<string>('🌤️');
+
+  readonly sizeClass = computed(() => `height: ${this.size()}px; width: ${this.size()}px;`);
 }
